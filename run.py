79ffdbe9abc3c3e706768f57c641f87a02b4682f
Edward Lee <edwardlee0323@gmail.com>
2016-06-15 13:45:41 +0000

    Added Seed for JSON testing

--- run.py
+++ run.py
@@ -1,10 +1,28 @@
 import os
 from posts import app
+from posts.models import Post
+from posts.database import session
 
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
+def seed():
+    content = """Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
+    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+    
+    for i in range(25):
+        posts = Post(
+            title="Test Entry #{}".format(i),
+            body=content
+            )
+        session.add(posts)
+    session.commit()
+
 if __name__ == '__main__':
+    seed()
     run()
 
